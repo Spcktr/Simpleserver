704a8085ec2acbb1fa68f1716bba71c9d7b82e27
spcktr <rocket155@gmail.com>
2018-04-21 02:11:57 +0000

    added simple routing

--- src/web/core.clj
+++ src/web/core.clj
@@ -1,13 +1,23 @@
 (ns web.core
-  (:require [org.httpkit.server :as s]))
+  (:require [org.httpkit.server :as s]
+            [compojure.core :refer [routes POST GET ANY]]))
 
-(defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "<h1> Hello World!</h1>"})
+(defn app []
+  (routes
+    (GET "/" [:as req]
+        {:status 200
+         :headers {"Content-Type" "text/html"}
+         :body "<h1> Hello from root route!</h1>"}
+        )
+     (GET "/:user-name" [user-name :as req]
+        {:status 200
+          :headers {"Content-Type" "text/html"}
+          :body (format "<h1> Hello from %s! </h1>" user-name)}
+      )
+    ))
 
 (defn create-server []
-  (s/run-server handler {:port 8080}))
+  (s/run-server (app) {:port 8080}))
 
 (defn stop-server [server]
   (server :timeout 100))
